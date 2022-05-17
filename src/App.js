74767eba975b6ab1aa5de1283545088cd6a90425
Vinicius Costa <91564654+VininCosta@users.noreply.github.com>
2022-05-17 13:36:26 -0300

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined, FacebookFilled, TwitterSquareFilled, GithubFilled } from '@ant-design/icons';
 
-function App() {
+const NormalLoginForm = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='form'>
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <h1>Login</h1> <br />
+      <Form.Item
+        name="username"
+        className='label'
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        className='label'
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <a className="forgot" href="">
+          Forgot password
         </a>
-      </header>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button> <br />
+        <a href="">register now!</a> <br /><br />
+        <p>Or Sign Up Using</p> 
+        <div className='icon'>
+        <FacebookFilled /> <TwitterSquareFilled /> <GithubFilled />
+        </div>
+      </Form.Item>
+    </Form>
     </div>
   );
-}
+};
 
-export default App;
+export default () => <NormalLoginForm />;
+   
+
